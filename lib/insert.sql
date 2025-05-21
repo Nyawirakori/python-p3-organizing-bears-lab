c95247142d14edbe4030480738f221de93d5e322
Joan Nyawira Kori <korijoan171@gmail.com>
2025-05-21 17:10:13 +0300

    SQL QUERIES

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES 
+("Mr. Chocolate", 12, "M", "Blue", "Melancholic", 1),
+("Rowdy", 12, "M", "Blue", "Melancholic", 1),
+("Tabitha", 11, "F", "Black", "Sanguine", 0),
+("Sergeant Brown", 1, "M", "Green", "Choleric", 0),
+("Melissa", 2, "F", "Blue", "Melancholic", 0),
+("Grinch", 5, "F", "Beige", "Phlegmatic", 1),
+("Wendy", 12, "M", "Blue", "Melancholic", 0),
+(NULL, 7, "M", "Purple", "Melancholic", 1);
